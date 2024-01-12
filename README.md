0bb4edc34f897c9e5563e6d1652f4b2fd2c97c71
redowan <redowan.nafi@gmail.com>
2024-01-12 23:06:55 +0100

    Run gofmt, refs #12

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <div align="center">
-Wandering dilettante with a flair for 1s and 0s <br>
+Wandering dilettante with a flair for 1s and 0s>>> <br>
 Find my musings on <a href="https://rednafi.com/" rel="me">rednafi.com</a>
 <div>
 
--- scripts/main_test.go
+++ scripts/main_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+
 const httpResponse = `
 <?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
